ec17957cca18e36c7f6c4ca47c6667e7c8f1db8a
Raphaellllllll <104317055+Raphaellllllll@users.noreply.github.com>
2024-04-09 15:22:35 +0200

    Ajout du profil
    profil pas encore fait à l'intérieur

--- src/vue/Profil.java
+++ src/vue/Profil.java
@@ -9,16 +9,16 @@ import java.util.ArrayList;
 import controller.*;
 import model.Film;
 
-public class AccueilMembre extends JFrame implements ActionListener {
+public class Profil extends JFrame implements ActionListener {
 
     private Controller controller;
     private JLabel titre, user;
-    private JButton quitter, disconnect, profil;
+    private JButton quitter, disconnect;
 
     private ArrayList<Film> list;
 
 
-    public AccueilMembre(Controller controller) {
+    public Profil(Controller controller) {
         this.controller = controller;
         controller.film();
         list = controller.getListFilm();
@@ -36,29 +36,28 @@ public class AccueilMembre extends JFrame implements ActionListener {
         //titre et user
         JPanel titrePanel = new JPanel(new GridLayout(1, 2));
         titrePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        titre = new JLabel("Cinema de Grenelle");
+        titre = new JLabel("Votre Profil : ");
         titre.setFont(new Font("Serif", Font.BOLD, 32));
         titre.setHorizontalAlignment(SwingConstants.LEFT);
         titrePanel.add(titre);
         user = new JLabel(controller.getUsername());
         user.setFont(new Font("Serif", Font.BOLD, 20));
-        user.setHorizontalAlignment(SwingConstants.RIGHT);
+        user.setHorizontalAlignment(SwingConstants.LEFT);
         titrePanel.add(user);
         add(titrePanel, BorderLayout.NORTH);
 
         //Deconection ou quitter etc
-        JPanel menu = new JPanel(new GridLayout(0, 3, 10, 3));
+        JPanel menu = new JPanel(new FlowLayout(FlowLayout.CENTER));
         menu.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 
         quitter = new JButton("Quitter");
         quitter.addActionListener(this);
         menu.add(quitter);
+
         disconnect = new JButton("Se déconnecter");
         disconnect.addActionListener(this);
         menu.add(disconnect);
-        profil = new JButton("Votre Profil");
-        profil.addActionListener(this);
-        menu.add(profil);
+
         add(menu, BorderLayout.SOUTH);
 
         //sections
@@ -93,12 +92,12 @@ public class AccueilMembre extends JFrame implements ActionListener {
         // Card card1 = new Card("a", "a");
 
 
-        for (int i = 0; i < list.size(); i++) {
+        /*for (int i = 0; i < list.size(); i++) {
             JPanel panelt = new JPanel();
             Card card = new Card(list.get(i).getTitre(), list.get(i).getTime(), list.get(i).getGenre(), list.get(i).getDescription(), list.get(i).getClassification(), list.get(i).getDate(), list.get(i).getPoster(), panelt, this.controller, list.get(i).getFilmId());
             panel.add(panelt);
 
-        }
+        }*/
 
 
         setVisible(true);
@@ -112,9 +111,10 @@ public class AccueilMembre extends JFrame implements ActionListener {
             list.clear();
             Form form = new Form(controller);
         }
-        else if (e.getSource() == profil) {
+        else if (e.getSource() == disconnect) {
+            this.dispose();
             list.clear();
-            Profil profil = new Profil(controller);
+            Form form = new Form(controller);
         }
 
 
